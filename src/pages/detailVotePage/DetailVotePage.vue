ed242e1362498345b12913a323cddce0a8de8c31
wintermaples <pg.wintermaples@gmail.com>
2020-04-27 14:48:38 +0900

    Created DetailVotePage

---
+++ src/pages/detailVotePage/DetailVotePage.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class DetailVotePageComponent extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import TopPageComponent from "@/pages/topPage/TopPage";
-import CreateVotePageComponent from "@/pages/createVotePage/createVotePage";
-import ListVotePageComponent from "@/pages/listVotePage/ListVotePage";
+import TopPageComponent from "@/pages/topPage/TopPage.vue";
+import CreateVotePageComponent from "@/pages/createVotePage/createVotePage.vue";
+import ListVotePageComponent from "@/pages/listVotePage/ListVotePage.vue";
+import DetailVotePageComponent from "@/pages/detailVotePage/DetailVotePage.vue";
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,7 @@ export const routes = [
   { path: '/', component: TopPageComponent },
   { path: '/createVote', component: CreateVotePageComponent },
   { path: '/list', component: ListVotePageComponent },
+  { path: '/detail/:id', component: DetailVotePageComponent },
 ];
 
 const router = new VueRouter({
