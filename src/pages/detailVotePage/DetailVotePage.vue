80308ab33312fbb2c5303651b473424f6b9d87f1
wintermaples <pg.wintermaples@gmail.com>
2020-04-28 20:23:17 +0900

    Implemented DetailVotePage design.

--- src/pages/detailVotePage/DetailVotePage.vue
+++ src/pages/detailVotePage/DetailVotePage.vue
@@ -5,16 +5,36 @@
     </div>
     <div id="voteTitle">{{ vote.title }}</div>
     <div id="voteDescription" class="multiline-text">{{ vote.description }}</div>
-    <div id="usernameContainer">{{ vote.creator }}</div>
-    <div id="createdAt">作成日時:&nbsp;{{ vote.created_at }}</div>
-    <div id="closingAt">締切:&nbsp;{{ vote.closing_at }}</div>
-    <div id="voteCount">投票数:&nbsp;{{ vote.vote_count }}</div>
-    <div id="voteButton"></div>
+    <!-- UserNameContainer Start -->
+    <div id="usernameContainer" class="right" v-if="vote.creator">
+      {{ vote.creator }}
+    </div>
+    <div id="usernameContainer" class="right" v-else>
+      <img src="/static/images/default-user-icon.png" alt="Anonymus" id="usericon">
+      <div id="username">匿名</div>
+    </div>
+    <!-- UserNameContainerENd -->
+    <div class="clear"></div>
+    <div id="createdAt" class="right">作成日時:&nbsp;{{ formatDate(vote.created_at) }}</div>
+    <div class="clear"></div>
+    <div id="closingAt" class="right">締切:&nbsp;{{ formatDate(vote.closing_at) }}</div>
+    <div class="clear"></div>
+    <div id="voteCount" class="right">投票数:&nbsp;{{ vote.vote_count }}</div>
+    <div class="clear"></div>
+    <div id="voteButtonContainer">
+      <router-link to id="voteButton">この投票に回答する</router-link>
+    </div>
     <div id="voteBorder"></div>
     <div id="votingResultsContainer">
       <div class="votingResultOfOneQuestion" v-for="(question, questionIndex) in vote.questions">
         <div class="question-title">Q{{ questionIndex + 1}}.&nbsp;{{ question.title }}</div>
-        <component :is="toVotingResultOfOneQuestionView(question)" :question="question" :votingResult="votingResults[questionIndex]" :index="questionIndex" />
+        <component
+          class="voting-result-of-one-question-view"
+          :is="toVotingResultOfOneQuestionView(question)"
+          :question="question"
+          :votingResult="votingResults[questionIndex]"
+          :index="questionIndex"
+        />
       </div>
     </div>
   </div>
@@ -53,4 +73,93 @@ export default class DetailVotePageComponent extends Vue {
 </script>
 
 <style lang="scss" scoped>
+#container {
+  max-width: 1024px;
+  margin: auto;
+}
+
+#voteTitle {
+  width: 100%;
+  border-bottom: 2px solid #7a7a7a;
+  margin-top: 50px;
+  font-size: 187.5%;
+  font-weight: bold;
+}
+
+#voteDescription {
+  margin: 20px auto;
+}
+
+#usernameContainer {
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#usericon {
+  width: 32px;
+  max-width: 32px;
+  max-height: 32px;
+  height: auto;
+  margin-right: 2em;
+}
+
+#username {
+  font-weight: bold;
+  font-size: 130%;
+}
+
+#createdAt,
+#closingAt,
+#voteCount {
+  font-size: 87.5%;
+  color: #606060;
+}
+
+#voteButtonContainer {
+  margin: 132px auto;
+  text-align: center;
+}
+
+#voteButton {
+  text-decoration: inherit;
+  border-radius: 4px;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+  padding: 1em 5em;
+  background-color: #4c7a34;
+  color: white;
+  font-weight: bold;
+
+  transition: box-shadow 0.2s, background-color 0.2s;
+
+  &:hover {
+    background-color: #5c8a44;
+    box-shadow: 5px 5px 7px rgba(0, 0, 0, 0.5);
+  }
+
+  &:active {
+    background-color: #3c6a24;
+    box-shadow: 3px 3px 1px rgba(0, 0, 0, 0.5);
+  }
+}
+
+#voteBorder {
+  width: 100%;
+  border-top: 1px solid #c0c0c0;
+}
+
+.votingResultOfOneQuestion {
+  margin-top: 75px;
+}
+
+.question-title {
+  font-size: 130%;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.voting-result-of-one-question-view {
+  width: 600px;
+}
 </style>
