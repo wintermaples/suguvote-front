2b2ae1ee3d0faaa544567cdecacf25f12d9edf52
wintermaples <pg.wintermaples@gmail.com>
2020-05-08 03:26:06 +0900

    Added footer. Changed BASE_URL for development.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
     <div id="header">
-      <div id="headerTitle"><router-link to="/">Suguvote</router-link></div>
+      <div id="headerTitle">
+        <router-link to="/">Suguvote</router-link>
+      </div>
       <div id="headerMenu"></div>
     </div>
     <div id="content">
       <transition name="router-transition" mode="out-in">
         <router-view></router-view>
       </transition>
+    </div>
+    <div id="footer">
+      <div style="white-space: ">
+        Made by
+        <a href="https://twitter.com/yoshi_yukky">@yoshi_yukky</a>.
+        <br />Suguvote is in development now(Alpha dev.1).
+      </div>
     </div>
   </div>
 </template>
@@ -48,12 +57,26 @@ export default class AppComponent extends Vue {}
   margin: auto;
 }
 
-.router-transition-enter-active, .router-transition-leave-active {
+.router-transition-enter-active,
+.router-transition-leave-active {
   transition: opacity 0.2s;
 }
 
-.router-transition-enter, .router-transition-leave-to {
+.router-transition-enter,
+.router-transition-leave-to {
   opacity: 0;
 }
 
+#footer {
+  $paddingX: 15%;
+  width: calc(100% - #{$paddingX} * 2);
+  height: 40px;
+  margin: 30px auto 0px auto;
+  padding: 15px $paddingX;
+  background-color: #f6f6f6;
+  color: #aaa;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
--- src/const/CommonConst.ts
+++ src/const/CommonConst.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_PAGE_SIZE: number = 20;
-export const BASE_URL: string = 'http://35.199.190.207:8080/';
+export const BASE_URL: string = 'http://localhost:8000/';
